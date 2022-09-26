af49f4f0ec3ed6b1811b94e0c097abf9609807f1
Karan Sharma <sharma510@gmail.com>
2022-09-27 00:47:59 +0530

    goint to sleep

---
+++ contracts/bet.sol
@@ -0,0 +1,15 @@
+pragma solidity 0.8.17;
+
+contract bet {
+    function betOnMatch(uint256 matchId, uint256 amount) public {
+
+    }
+}
+
+contract matches{
+    struct schedule {
+        unit256 matchId;
+        String date;
+        teams []
+    }
+}
