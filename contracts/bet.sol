73633fd3beedb4576a6e7f98f6e3fa32a2f25b6f
Karan Sharma <sharma510@gmail.com>
2022-09-28 10:03:50 +0530

    bet on match method added

--- contracts/bet.sol
+++ contracts/bet.sol
@@ -3,12 +3,13 @@ pragma solidity 0.8.17;
 
 contract bet {
     // uint256 constant minimumbet = 0.004;
-    uint256 constant minimumbet = 4000000000000000;
+    uint256 constant minimumBet = 4000000000000000;
 
     struct bet{
         uint matchId;
         address player;
         uint prediction;
+        uint bet_time;
     }
 
     struct matchBet{
@@ -21,15 +22,108 @@ contract bet {
         uint total_tie;
         uint total_tie_amount;
         mapping(address => bet) bets;
+        mapping(address => bool) players;
     }
 
-    struct bets{
+    // storing all match bet detials
+    mapping(uint => matchBet) allBets;
+    mapping(uint => bool) startedBet;
+    uint total_games=0;
 
+    matches game = new matches();
+
+    // checks if player had already bet on given match
+    function alreadyBet(uint matchId) public view returns(bool) {
+        return allBets[matchId].players[msg.sender];
     }
 
-    // mapping()
-    function betOnMatch(uint256 matchId, uint256 amount, uint prediction) public {
+
+    function betOnMatch(uint256 matchId, uint256 amount, uint prediction) public payable {
+        // player must not bet more than once on a particuar match
+        require(!allBets[matchId].players[msg.sender], "you have alredy placed a bet on this match");
+        require(block.timestamp <= (game.getMatch(matchId).endDate - 86400), "The duration of bettig on this Match is over." );
+
+        // bet amount should be equal to or greater than minimumBet
+        require(amount >= minimumBet, "Please send atleast minimum amount to bet"); // ether in wei
         
+
+        bet memory bet1 = bet(matchId, msg.sender, prediction, block.timestamp);
+
+        // first bet on a match
+        if(!startedBet[matchId]){
+            startedBet[matchId] = true;
+            total_games++;
+
+            matchBet storage match1 = allBets[matchId];
+
+            match1.matchId = matchId;
+            match1.totalCount = 1;
+            match1.bets[msg.sender] = bet1;
+            match1.players[msg.sender] = true;
+
+            if(prediction == 1){
+                match1.total_team1_win = 1;
+                match1.total_team1_amount = amount;
+                match1.total_team2_win = 0;
+                match1.total_team2_amount = 0;
+                match1.total_tie = 0;
+                match1.total_tie_amount = 0;
+
+            }else if(prediction == 2){
+                match1.total_team1_win = 0;
+                match1.total_team1_amount = 0;
+                match1.total_team2_win = 1;
+                match1.total_team2_amount = amount;
+                match1.total_tie = 0;
+                match1.total_tie_amount = 0;
+
+            }else if(prediction == 0){
+                match1.total_team1_win = 0;
+                match1.total_team1_amount = 0;
+                match1.total_team2_win = 0;
+                match1.total_team2_amount = 0;
+                match1.total_tie = 1;
+                match1.total_tie_amount = amount;
+            }
+
+
+            // if(prediction==1){
+            //     match1 = matchBet(matchId,1,1,amount,0,0,0,0,bet1, true);
+            // }
+            // else if(prediction==2){
+            //     match1 = matchBet(matchId,1,0,0,1,amount,0,0,bet1, true);
+            // }
+            // else if(prediction=0){
+            //     match1 = matchBet(matchId,1,0,0,0,0,1,amount,bet1, true);
+            // }
+
+            // allBets[matchId]  = match1;
+        }else{
+            matchBet storage match2;
+            match2 = allBets[matchId];
+
+            if(matchId == match2.matchId){
+                match2.matchId = matchId;
+                match2.totalCount += 1;
+                match2.bets[msg.sender] = bet1;
+                match2.players[msg.sender] = true;
+
+                if(prediction == 1){
+                    match2.total_team1_win += 1;
+                    match2.total_team1_amount += amount;
+
+                }else if(prediction == 2){
+                    match2.total_team2_win += 1;
+                    match2.total_team2_amount += amount;
+
+                }else if(prediction == 0){
+                    match2.total_tie += 1;
+                    match2.total_tie_amount += amount;
+                }
+            }          
+            
+        }
+
     }
 }
 
